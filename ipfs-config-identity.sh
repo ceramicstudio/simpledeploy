b05be273a58a2912d90818987491ba931654eb47
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-03-15 15:41:49 -0400

    fix: remove jq installation
    From ipfs config script

--- ipfs-config-identity.sh
+++ ipfs-config-identity.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-sudo apt-get install jq
-
 FILE=$1
 PEER=$2
 PRIV=$3
@@ -10,4 +8,3 @@ TEMP=$(mktemp)
 jq --arg PRIV "$PRIV" '.Identity.PrivKey = $PRIV' $FILE > "$TEMP" && mv "$TEMP" $FILE || exit 1
 jq --arg PEER "$PEER" '.Identity.PeerID = $PEER' $FILE > "$TEMP" && mv "$TEMP" $FILE || exit 1
 cat $FILE | jq '.Identity'
-
