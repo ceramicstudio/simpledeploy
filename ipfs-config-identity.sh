a37a73fa9cd91751ae30b0405c8e08cebe04e721
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-03-15 13:22:47 -0400

    feat: add ipfs config identity script

---
+++ ipfs-config-identity.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt-get install jq
+
+FILE=$1
+PEER=$2
+PRIV=$3
+TEMP=$(mktemp)
+
+jq --arg PRIV "$PRIV" '.Identity.PrivKey = $PRIV' $FILE > "$TEMP" && mv "$TEMP" $FILE || exit 1
+jq --arg PEER "$PEER" '.Identity.PeerID = $PEER' $FILE > "$TEMP" && mv "$TEMP" $FILE || exit 1
+cat $FILE | jq '.Identity'
+
