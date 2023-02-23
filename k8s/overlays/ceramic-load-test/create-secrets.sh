400f42a1482180068da4aac17f642c7e04cab20e
Ben Wilson <ben@3box.io>
2023-02-23 15:37:04 -0500

    k8s overlay for ceramic/composedb load testing

---
+++ k8s/overlays/ceramic-load-test/create-secrets.sh
@@ -0,0 +1,6 @@
+kubectl create secret generic postgres-auth \
+  --namespace ceramic-load-test \
+  --from-literal=username=ceramic --from-literal=password=$(openssl rand -hex 20)
+kubectl create secret generic ceramic-admin \
+  --namespace ceramic-load-test \
+  --from-literal=private-key=$(openssl rand -hex 32)
