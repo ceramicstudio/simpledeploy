a9bc72b3e15349abd11b969669eb41fe6a461882
Ben Wilson <ben@3box.io>
2024-04-09 08:26:27 -0400

    feat: manifests for prod

---
+++ k8s/base/ceramic-one-prod/scripts/create-secrets.sh
@@ -0,0 +1,7 @@
+kubectl create secret generic ceramic-admin \
+  --from-literal=private-key=$(openssl rand -hex 32)
+kubectl create secret generic ceramic-postgres-auth \
+  --from-literal=db=ceramic \
+  --from-literal=host=postgres \
+  --from-literal=password=ceramic \
+  --from-literal=username=ceramic
