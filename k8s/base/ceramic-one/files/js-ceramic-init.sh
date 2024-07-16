e7d65c4cd91bc480b09ac982b1e66886e68acc7a
Ben Wilson <ben@3box.io>
2024-07-16 08:02:09 -0400

    chore: rename k8s ceramic-one manifests from rust

--- k8s/base/ceramic-one/files/js-ceramic-init.sh
+++ k8s/base/ceramic-one/files/js-ceramic-init.sh
@@ -3,8 +3,8 @@
 set -eo pipefail
 
 export MY_POD_INDEX=$(echo $MY_POD_NAME | grep -o '[0-9]\+$')
-# Assuming http://rust-ceramic-1:5001 format
-export CERAMIC_IPFS_HOST=http://rust-ceramic-${MY_POD_INDEX}:5001
+# Assuming http://ceramic-one-1:5001 format
+export CERAMIC_IPFS_HOST=http://ceramic-one-${MY_POD_INDEX}:5001
 export CERAMIC_ADMIN_DID=$(composedb did:from-private-key ${CERAMIC_ADMIN_PRIVATE_KEY})
 
 CERAMIC_ADMIN_DID=$CERAMIC_ADMIN_DID envsubst < /js-ceramic-init/daemon-config.json > /config/daemon-config.json
