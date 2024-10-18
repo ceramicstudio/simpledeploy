d585e5d321e1efebcadb9cb5c9336d18c403addd
Ben Wilson <ben@3box.io>
2024-10-18 16:47:39 -0400

    feat: added ceramic-one-mainnet k8s example

---
+++ k8s/ceramic-one-mainnnet/files/js-ceramic-init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eo pipefail
+
+export MY_POD_INDEX=$(echo $MY_POD_NAME | grep -o '[0-9]\+$')
+# Assuming http://ceramic-one-1:5101 format
+export CERAMIC_IPFS_HOST=http://ceramic-one-${MY_POD_INDEX}:5101
+export CERAMIC_ADMIN_DID=$(composedb did:from-private-key ${CERAMIC_ADMIN_PRIVATE_KEY})
+
+CERAMIC_ADMIN_DID=$CERAMIC_ADMIN_DID envsubst < /js-ceramic-init/daemon-config.json > /config/daemon-config.json
